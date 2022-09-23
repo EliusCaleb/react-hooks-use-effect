5cc8a769ed3176571b45f0486cc4d4b818fa5c0c
ELIUS <eliuswanyoro@gmail.com>
2022-09-23 13:58:16 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import DogPics from "./DogPics";
 
+//ReactDOM.render(<DogPics />, document.getElementById("root"));// for Dogs Pics
 ReactDOM.render(<App />, document.getElementById("root"));
